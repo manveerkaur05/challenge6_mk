6cc38a2c758cfe26efdd4dce1d32410a8f91426f
manveerkaur05 <manveerkaur4@rrc.ca>
2024-02-27 17:04:33 -0600

    Added alphabetized and missing_email actions/views

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,19 @@
+# app/controllers/customers_controller.rb
+class CustomersController < ApplicationController
+    def index
+      @customers = Customer.all
+    end
+
+    def alphabetized
+        @customers = Customer.order(:full_name)
+      end
+
+      def missing_email
+        @customers = Customer.where(email_address: [nil, ''])
+      end
+      
+      
+
+
+  end
+  
